149928c5a97c5ce65426bfbd903604c2e1a67f09
Joel <joel28leiva@gmail.com>
2022-01-23 13:26:51 -0500

    Configuracion de Hooks y pom

---
+++ src/test/java/Definitions/Hooks.java
@@ -0,0 +1,26 @@
+package Definitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hooks {
+
+    //variable de incio
+    public static WebDriver driver;
+
+
+    @Before
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public static void tearDown(){//metodo
+        driver.manage().deleteAllCookies();
+        driver.close();
+    }
+}
